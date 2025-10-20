d9765be1124fad538d5719af72b387a3513fc3f5
Scott Griffiths <alotoffluffyhair68@googlemail.com>
2025-10-20 22:09:44 +0100

    text

--- page/welcome_page.py
+++ page/welcome_page.py
@@ -7,10 +7,10 @@ st.image("images/DEFINEIO.png")
 
 # Welcome page content
 st.markdown('''
-## Welcome to DEFINE-IO
+# Welcome to DEFINE-IO
 
-# Dear all, 
+## Dear all, 
 
-# Thank you very much for attending the DEFINE-IO consensus meeting, please take a seat, and we are due to start shortly. 
+## Thank you very much for attending the DEFINE-IO consensus meeting, please take a seat, and we are due to start shortly. 
             ''')
 st.divider()
