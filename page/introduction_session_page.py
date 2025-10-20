c7eaaffb611408b5083bec00b8853e51ef1b2384
Scott Griffiths <alotoffluffyhair68@googlemail.com>
2025-10-20 22:08:11 +0100

    minor text edits

--- page/introduction_session_page.py
+++ page/introduction_session_page.py
@@ -6,5 +6,15 @@ st.image("images/DEFINEIO.png")
 
 # Intro page content
 st.markdown('''
-# TBD
+            Today's rundown:
+            - Welcome - Prof Tze Min Wah
+            - Introduction/ run down - Vinson Chan
+            - Summary of round 2 results and findings - Vinson Chan/ Scott Griffiths
+            - Round 3 voting +/- Discussions - all
+            - Break
+            - Summary of Round 3 results and findings - Vinson Chan/ Scott Griffiths
+            - MCDA priotisation - all 
+            - MCDA swing weighting - all
+            - Closing - Prof Tze Min Wah
             ''')
+st.divider()
--- page/welcome_page.py
+++ page/welcome_page.py
@@ -7,22 +7,10 @@ st.image("images/DEFINEIO.png")
 
 # Welcome page content
 st.markdown('''
-# Welcome to DEFINE-IO
+## Welcome to DEFINE-IO
 
-Dear all, 
+#Dear all, 
 
-Thank you very much for attending the DEFINE-IO consensus meeting, please take a seat, and we are due to start shortly. 
+#Thank you very much for attending the DEFINE-IO consensus meeting, please take a seat, and we are due to start shortly. 
             ''')
 st.divider()
-st.markdown('''
-            Today's rundown:
-            - Welcome - Prof Tze Min Wah
-            - Introduction/ run down - Vinson Chan
-            - Summary of round 2 results and findings - Vinson Chan/ Scott Griffiths
-            - Round 3 voting +/- Discussions - all
-            - Break
-            - Summary of Round 3 results and findings - Vinson Chan/ Scott Griffiths
-            - MCDA priotisation - all 
-            - MCDA swing weighting - all
-            - Closing - Prof Tze Min Wah
-            ''')
